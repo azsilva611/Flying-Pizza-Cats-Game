e61c17c8f3fb55627f0efb21fde33dc20063f046
Aaron Silva <azsilva611@gmail.com>
2023-07-29 03:52:40 -0700

    created player movement and player projectiles

---
+++ Assets/Scripts/Slice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slice : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10.0f;
+
+
+    void Update()
+    {
+        MoveSelf();
+    }
+
+    void MoveSelf()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        if (transform.position.y > 8)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
